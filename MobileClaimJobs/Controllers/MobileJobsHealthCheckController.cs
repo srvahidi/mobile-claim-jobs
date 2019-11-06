5ec5db19ca63d5ea16bceda65e2b40af61ed223b
Your Name <you@example.com>
2019-11-06 14:40:17 -0600

    Test

---
+++ MobileClaimJobs/Controllers/MobileJobsHealthCheckController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MobileClaimJobs.Controllers
+{
+    [Route("api/MobileJobsHealthCheck")]
+    [ApiController]
+    public class MobileJobsHealthCheckController : Controller
+    {
+        public ActionResult Working()
+        {
+            return Ok();
+        }
+    }
+}
